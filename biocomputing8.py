42c4f6bb1f71f126e95e37b3c6cc1656e8b63a61
Caitlin Harrington <cnharrington97@gmail.com>
2018-10-28 14:39:24 -0400

    second commit

--- biocomputing8.py
+++ biocomputing8.py
@@ -5,7 +5,7 @@ Created on Sun Oct 28 14:00:31 2018
 
 @author: charrington
 """
-
+# 1
 import pandas
 scores=pandas.read_csv("UWvMSU_1-22-13.txt",header=0,sep="\t")
 
@@ -13,6 +13,7 @@ scoresframe=pandas.DataFrame(columns=['time','UW','MSU'])
 
 UWscore=0
 MSUscore=0
+
 
 for i in range(0,len(scores)):
     if scores.iloc[i,1]=="UW":
@@ -23,4 +24,39 @@ for i in range(0,len(scores)):
     scoresframe.loc[len(scoresframe)]=[scores.iloc[i,0],UWscore,MSUscore]
     
     
+import matplotlib.pyplot as plt
+plt.plot(time,UWscore,'r-',time,MSUscore,'g-')
+
+#2 guess my number
+
+import numpy
+
+list=range(1,100)
+
+def numbergame():
+    numberchoice=numpy.random.choice(list)
+    print ('Im thinking of a number 1-100...')
+    user_guess=raw_input('guess:')
+    if user_guess>numberchoice:
+        print ('Lower')
+        raw_input(['guess:'])
+    elif user_guess<numberchoice:
+        print ('Lower')
+        raw_input(['guess:'])
+    else:
+        print ('Correct!')
+        break
     
+        
+
+
+
+
+
+
+
+
+
+
+
+
